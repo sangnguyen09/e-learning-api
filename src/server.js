b4f22f67a881e216fcb0484d09093a9a92532a5b
Sang Nguyen <sang7817@gmail.com>
2019-08-15 01:37:52 -0700

    [1.0.0] add routes and controller

--- src/server.js
+++ src/server.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import ConnectDB from './config/connectDB'
-import configViewEngine from './config/viewEngine'
+import configViewEngine from './config/viewEngine';
+import initRoutes from './routes/web'
 
 let app = express();
 
@@ -10,12 +11,9 @@ ConnectDB()
 // config view engine
 configViewEngine(app);
 
-app.get('/', (req,res)=>{  
-   return res.render("main/master") // been file cau hình đã định nghĩa đường dẫn src/view
-})
-app.get('/login-register', (req,res)=>{  
-    return res.render("auth/loginRegister");
- })
+// init routes
+initRoutes(app)
+
 app.listen(process.env.APP_PORT,process.env.APP_HOST, ()=>{
     console.log(`Hello, running at ${process.env.APP_HOST}:${process.env.APP_PORT}/`)
 })
