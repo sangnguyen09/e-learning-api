ee419b9bd13fbb3c00c3bd8663cfd7f1a55e1c30
sanglight <nguyenthanhsang7817@gmail.com>
2019-08-16 21:42:13 -0700

    [1.0.0] update avatar backend

---
+++ src/controllers/userController.js
@@ -0,0 +1,69 @@
+import multer from 'multer';
+import {
+    app
+} from '../config/app';
+import {
+    transErrors,
+    transSuccess
+} from '../../lang/vi';
+import randomId from 'random-id'
+import {
+    user
+} from '../services';
+import fsExtra from 'fs-extra' // xu ly file ho tro bat dong bo
+
+let storageAvatar = multer.diskStorage({
+    destination: (req, file, callback) => {
+        callback(null, app.avatar_directory)
+    },
+    filename: (req, file, callback) => {
+        let math = app.avatar_type;
+        if (math.indexOf(file.mimetype) === -1) {
+            return callback(transErrors.avatar_type, null)
+        }
+
+        let avatarName = `${Date.now()}-${randomId(5, 'aA0')}-${file.originalname}`
+        callback(null, avatarName)
+    }
+});
+let avatarUploadFile = multer({
+    storage: storageAvatar,
+    limits: {
+        fileSize: app.avatar_limit_size
+    }
+}).single('avatar') //avatar trung voi formData o file js  formData.append('avatar', fileData);
+
+let updateAvatar = (req, res) => {
+
+    avatarUploadFile(req, res, async (err) => {
+        if (err) {
+            console.log(err);
+            if (err.message) {
+                return res.status(500).send(transErrors.avatar_size)
+            }
+            return res.status(500).send(err)
+        }
+        try {
+            let updateUserItem = {
+                avatar: req.file.filename,
+                updatedAt: Date.now()
+            }
+            // update user
+            let userUpdate = await user.updateUser(req.user._id, updateUserItem)
+
+            // renmove avatar old
+            await fsExtra.remove(`${app.avatar_directory}/${userUpdate.avatar}`)
+            let result = {
+                message: transSuccess.avatar_updated,
+                imageSrc: `/images/users/${req.file.filename}`
+            }
+            return res.status(200).send(result)
+
+        } catch (error) {
+            return res.status(500).send(error)
+        }
+    })
+}
+module.exports = {
+    updateAvatar
+}
--- src/public/js/updateUser.js
+++ src/public/js/updateUser.js
@@ -2,91 +2,114 @@
  let userInfo = {};
  let originAvatarSrc = null
 
-function updateUserInfo() {
-    $("#input-change-avatar").bind('change', function(){
-        let fileData = $(this).prop('files')[0];
-        let math =['image/png','image/jpg','image/jpeg',];
-        let limit = 10485766;// byte= 1M
-        if ($.inArray(fileData.type, math )===-1) {
-            alertify.notify("Kiểu file không hợp lệ, chỉ chấp nhận jpg & png", "error",7)
-            $(this).val(null);
-            return 
-        }
-        if (fileData.size > limit) {
-            alertify.notify("Ảnh upalod tối đa cho phép là 1MB", "error",7)
-            $(this).val(null);
-            return 
-        }
-        if (typeof (FileReader )!=="undefined") {
-            let imagePreview =$('#image-edit-profile');
-            imagePreview.empty();
-            let fileReader = new FileReader();
-            fileReader.onload = function (element) {
-                $('<img>',{
-                    'src': element.target.result,
-                    'class':'avatar img-circle',
-                    'alt':'avatar',
-                    'id':'user-modal-avatar'
-                }).appendTo(imagePreview);
-            }
-            imagePreview.show();
-            fileReader.readAsDataURL(fileData);
+ function updateUserInfo() {
+     $("#input-change-avatar").bind('change', function () {
+         let fileData = $(this).prop('files')[0];
+         let math = ['image/png', 'image/jpg', 'image/jpeg', ];
+         let limit = 1048576; // byte= 1M
+         if ($.inArray(fileData.type, math )===-1) {
+             alertify.notify("Kiểu file không hợp lệ, chỉ chấp nhận jpg & png", "error",7)
+             $(this).val(null);
+             return 
+         }
+         if (fileData.size > limit) {
+             alertify.notify("Ảnh upalod tối đa cho phép là 1MB", "error",7)
+             $(this).val(null);
+             return 
+         }
+         if (typeof (FileReader) !== "undefined") {
+             let imagePreview = $('#image-edit-profile');
+             imagePreview.empty();
+             let fileReader = new FileReader();
+             fileReader.onload = function (element) {
+                 $('<img>', {
+                     'src': element.target.result,
+                     'class': 'avatar img-circle',
+                     'alt': 'avatar',
+                     'id': 'user-modal-avatar'
+                 }).appendTo(imagePreview);
+             }
+             imagePreview.show();
+             fileReader.readAsDataURL(fileData);
 
-            let formData = new FormData();
-            formData.append('avatar', fileData);
-            userAvatar = formData
-        }else{
-            alertify.notify("Trình duyệt của bạn không hỗ trợ FileReader", "error",7)
-        }
-    })
-    $('#input-change-username').bind('change', function(){
-        userInfo.username = $(this).val()
-    })
-    $('#input-change-gender-male').bind('click', function(){
-        userInfo.gender = $(this).val()
-    })
+             let formData = new FormData();
+             formData.append('avatar', fileData);
+             userAvatar = formData
+             console.log(userAvatar.get('avatar'));
 
-    $('#input-change-gender-female').bind('click', function(){
-        userInfo.gender = $(this).val()
-    })
-    $('#input-change-phone').bind('change', function(){
-        userInfo.phone = $(this).val()
-    })
-    $('#input-change-address').bind('change', function(){
-        userInfo.address = $(this).val()
-    })
-}
-$(document).ready(function(){
-    updateUserInfo();
-    originAvatarSrc =$('#user-modal-avatar').attr('src');
+         } else {
+             alertify.notify("Trình duyệt của bạn không hỗ trợ FileReader", "error", 7)
+         }
+     })
+     $('#input-change-username').bind('change', function () {
+         userInfo.username = $(this).val()
+     })
+     $('#input-change-gender-male').bind('click', function () {
+         userInfo.gender = $(this).val()
+     })
 
-    $('#input-btn-update-user').bind('click', function () {
-        if ($.isEmptyObject(userInfo) && !userAvatar) {
-            alertify.notify("Bạn phải thay đổi thông tin trước khi cập nhật dữ liệu", "error",7)
-            return 
-        }
-        // goij toi api bang ajax
-        $.ajax({
-            url: '/user/update-avatar',
-            type:'put',
-            cache: false,
-            contentTypt: false,
-            processData:false,
-            data:userAvatar,
-            success: function (result) {
-                
-            },
-            error: function (err) {
-                
-            }
-        })
-        // console.log(userAvatar);
-        // console.log(userInfo);
-        
-    })
-    $('#input-btn-cancle-update-user').bind('click', function () {
-        userAvatar =null
-        userInfo = {}
-        $('#user-modal-avatar').attr('src', originAvatarSrc)
-    })
-})
+     $('#input-change-gender-female').bind('click', function () {
+         userInfo.gender = $(this).val()
+     })
+     $('#input-change-phone').bind('change', function () {
+         userInfo.phone = $(this).val()
+     })
+     $('#input-change-address').bind('change', function () {
+         userInfo.address = $(this).val()
+     })
+ }
+
+ $(document).ready(function () {
+     updateUserInfo();
+     originAvatarSrc = $('#user-modal-avatar').attr('src');
+
+    
+     $('#input-btn-update-user').bind('click', function () {
+         if ($.isEmptyObject(userInfo) && !userAvatar) {
+             alertify.notify("Bạn phải thay đổi thông tin trước khi cập nhật dữ liệu", "error", 7)
+             return
+         }
+         // goij toi api bang ajax
+         $.ajax({
+             url: '/user/update-avatar',
+             type: 'put',
+             cache: false,
+             contentType: false,
+             processData: false,
+             data: userAvatar,
+             success: function (result) {
+                 console.log(result);
+                       // Display success
+                       $('.user-modal-alert-success').find('span').text(result.message)
+                       $('.user-modal-alert-success').css('display', "block")
+
+                       // update avatar tai thanh n
+                       $('#navbar-avatar').attr('src',result.imageSrc)
+
+                       // update origin avatar src
+                       originAvatarSrc =result.imageSrc
+                       //reset all
+                       $('#input-btn-cancle-update-user').click()
+             },
+             error: function (err) {
+                 console.log(err)
+                 // Display errors
+                 $('.user-modal-alert-error').find('span').text(err.responseText)
+                 $('.user-modal-alert-error').css('display', "block")
+
+                  //reset all
+                 $('#input-btn-cancle-update-user').click()
+             }
+         })
+         // console.log(userAvatar); 
+         // console.log(userInfo);
+
+     })
+   
+     $('#input-btn-cancle-update-user').bind('click', function () {
+         userAvatar = null
+         userInfo = {}
+         $("#input-change-avatar").val(null)
+         $('#user-modal-avatar').attr('src', originAvatarSrc)
+     })
+ })
