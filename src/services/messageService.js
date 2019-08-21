8a43cc4602383c8811753b9e94c596174161d160
SangNguyen <sang7817@gmail.com>
2019-08-21 16:44:27 +0700

    [1.0.0] fix bug sort conversation and bug display

--- src/services/messageService.js
+++ src/services/messageService.js
@@ -4,30 +4,32 @@ import ChatGroupModel from '../models/chatGroupModel';
 import _ from 'lodash'
 
 const LIMIT_CONVERSATIONS_TAKEN = 10
-export const getAllConversationItems =(currentUserId)=>{
-	return new Promise(async (resolve,reject)=>{
+export const getAllConversationItems = (currentUserId) => {
+	return new Promise(async (resolve, reject) => {
 		try {
-            let contacts = await ContactModel.getContacts(currentUserId, LIMIT_CONVERSATIONS_TAKEN)
-            let userConversationsPromise = contacts.map(async contact => {
-                if (contact.contactId == currentUserId) { // so sanh 2 dau =,currentUserId la object
+			let contacts = await ContactModel.getContacts(currentUserId, LIMIT_CONVERSATIONS_TAKEN)
+			let userConversationsPromise = contacts.map(async contact => {
+				if (contact.contactId == currentUserId) { // so sanh 2 dau =,currentUserId la object
 					let getUserContact = await UserModel.getNormalUserDataById(contact.userId);
-					getUserContact.createdAt = contact.createdAt
+					getUserContact.updatedAt = contact.updatedAt
 					return getUserContact
-                } else {
-                    return await UserModel.getNormalUserDataById(contact.contactId);
-                }
+				} else {
+					let getUserContact = await UserModel.getNormalUserDataById(contact.userId);
+					getUserContact.updatedAt = contact.updatedAt
+					return getUserContact
+				}
 			})
-			let usersConversations =await Promise.all(userConversationsPromise)
+			let usersConversations = await Promise.all(userConversationsPromise)
 			let groupConversations = await ChatGroupModel.getChatGroups(currentUserId, LIMIT_CONVERSATIONS_TAKEN)
 			let allConversations = usersConversations.concat(groupConversations);
-			allConversations =_.sortBy(allConversations,(item)=> -item.createdAt)
-            resolve( {
+			allConversations = _.sortBy(allConversations, (item) => -item.updatedAt)
+			resolve({
 				usersConversations,
 				groupConversations,
 				allConversations,
 			})
-        } catch (error) {
-            reject(error)
-        }
+		} catch (error) {
+			reject(error)
+		}
 	})
 }
