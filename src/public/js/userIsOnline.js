475c5b902d8ed8258e19edce0b7c13feb25de6a8
SangNguyen <sang7817@gmail.com>
2019-08-25 00:51:22 +0700

    [1.0.0] user is online

---
+++ src/public/js/userIsOnline.js
@@ -0,0 +1,19 @@
+// step1:
+socket.on('server_send_listUsers_online', function(listUserIds){
+    console.log(listUserIds);
+    listUserIds.forEach(userId =>{
+        $(`.person[data-chat=${userId}]`).find('div.dot').addClass('online');
+        $(`.person[data-chat=${userId}]`).find('div>img').addClass('avatar-online');
+    })
+})
+// step2: khi một thăng online thì hiên lên cho thằng khác
+socket.on('server_send_user_online', function(userId){
+        $(`.person[data-chat=${userId}]`).find('div.dot').addClass('online');
+        $(`.person[data-chat=${userId}]`).find('div>img').addClass('avatar-online');
+})
+
+// step3: khi một thăng offline
+socket.on('server_send_user_offline', function(userId){
+        $(`.person[data-chat=${userId}]`).find('div.dot').removeClass('online');
+        $(`.person[data-chat=${userId}]`).find('div>img').removeClass('avatar-online');
+})
