b95b507a64eeb1779b66efe30a9bd6015cf9312f
SangNguyen <sang7817@gmail.com>
2019-08-28 22:25:17 +0700

    deploy heroki

--- src/config/connectDB.js
+++ src/config/connectDB.js
@@ -8,9 +8,8 @@ let connectDb = ()=>{
 
     // mongodb://localhost:27017/awesome_chat
     //let URI =`${process.env.DB_CONNECTION}://${process.env.DB_HOST}:${process.env.DB_PORT}/${process.env.DB_NAME}`;
-    let URI =`mongodb://sang:sang7817@awesome-chat-2ccbq.mongodb.net/test?retryWrites=true&w=majority`;
 
-    return  mongoose.connect('mongodb://sang:sang7817@awesome-chat-shard-00-00-2ccbq.mongodb.net:27017,awesome-chat-shard-00-01-2ccbq.mongodb.net:27017,awesome-chat-shard-00-02-2ccbq.mongodb.net:27017/awesome-chat?ssl=true&replicaSet=awesome-chat-shard-0&authSource=admin&retryWrites=true&w=majority', {useNewUrlParser: true});
+    return  mongoose.connect('mongodb://sang:sang7817@awesome-chat-shard-00-00-2ccbq.mongodb.net:27017,awesome-chat-shard-00-01-2ccbq.mongodb.net:27017,awesome-chat-shard-00-02-2ccbq.mongodb.net:27017/awesome-chat?ssl=true&replicaSet=awesome-chat-shard-0&authSource=admin&retryWrites=true&w=majority', {useMongoClient:true});
   // mongoose.connect(URI,{useNewUrlParser:true})
 }
 
